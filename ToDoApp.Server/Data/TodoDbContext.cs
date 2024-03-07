69db2d336a21012f158506f1dc159a6bd6d47023
User <User@DESKTOP-OEN81U5>
2024-03-07 08:59:27 +0300

    Добавьте файлы проекта.

---
+++ ToDoApp.Server/Data/TodoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using ToDoApp.Server.Models;
+
+namespace ToDoApp.Server.Data
+{
+    public class TodoDbContext : DbContext
+    {
+        public TodoDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Todo> Todos { get; set; }
+
+    }
+}
