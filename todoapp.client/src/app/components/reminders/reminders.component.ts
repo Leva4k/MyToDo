b2c0f3b49548126f4907e64ea7361777bd656161
Leva4k <levanax@mail.ru>
2024-03-14 21:53:22 +0500

    Напоминания

--- todoapp.client/src/app/components/reminders/reminders.component.ts
+++ todoapp.client/src/app/components/reminders/reminders.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../todos/services/todo.service';
+import { Todo } from '../todos/models/todo.model';
 
 @Component({
   selector: 'app-reminders',
   templateUrl: './reminders.component.html',
   styleUrl: './reminders.component.css'
 })
-export class RemindersComponent {
+export class RemindersComponent implements OnInit {
+  todayDate: Date = new Date();
+  todos: Todo[] = [];
+  constructor(private todoService: TodoService) { }
 
+  ngOnInit(): void {
+    this.todayDate.setDate(this.todayDate.getDate() + 1);
+    this.todayDate.setHours(0, 0, 0, 0);
+    this.getAllTodos();
+  }
+  getAllTodos() {
+    this.todoService.getAllTodos()
+      .subscribe({
+        next: (todos) => {
+          this.todos = todos;
+        }
+      });
+  }
 }
+
+
