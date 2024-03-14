542465599580f4c711fe6a3f04a8da7ee2a8a281
Leva4k <levanax@mail.ru>
2024-03-14 22:10:26 +0500

    Добавила напоминания

--- todoapp.client/src/app/components/reminders/reminders.component.ts
+++ todoapp.client/src/app/components/reminders/reminders.component.ts
@@ -8,13 +8,13 @@ import { Todo } from '../todos/models/todo.model';
   styleUrl: './reminders.component.css'
 })
 export class RemindersComponent implements OnInit {
-  todayDate: Date = new Date();
+  /*todayDate: Date = new Date();*/
   todos: Todo[] = [];
   constructor(private todoService: TodoService) { }
 
   ngOnInit(): void {
-    this.todayDate.setDate(this.todayDate.getDate() + 1);
-    this.todayDate.setHours(0, 0, 0, 0);
+    //this.todayDate.setDate(this.todayDate.getDate() + 1);
+    //this.todayDate.setHours(0, 0, 0, 0);
     this.getAllTodos();
   }
   getAllTodos() {
@@ -25,6 +25,19 @@ export class RemindersComponent implements OnInit {
         }
       });
   }
+
+  // метод фильтрации напоминаний на 3 дня
+  getRecentTodos() {
+    const today = new Date();
+    const fiveDaysFromNow = new Date();
+    fiveDaysFromNow.setDate(today.getDate() + 3);
+
+    return this.todos.filter(todo => {
+      const todoDate = new Date(todo.createdDate);
+      return todoDate >= today && todoDate <= fiveDaysFromNow;
+    });
+  }
 }
+
 
 
